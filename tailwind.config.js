5eaa222c24ffae041c2ba9e0cf86f6e7512641d9
Alex-mfoniso <145561709+Alex-mfoniso@users.noreply.github.com>
2024-10-10 04:40:40 +0100

    uploaded new files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      backgroundImage: {
+        'carnival-hero': "url('./img/carnival-hero.jpg')",
+      },
     },
   },
   plugins: [],
